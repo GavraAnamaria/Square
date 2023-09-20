26f16b714228caec1788d3e4add2978986a6000a
Anamaria Gavra <gavra.anamaria@yahoo.com>
2023-09-20 11:53:08 +0300

    commit2

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
-
-let p1 = document.getElementById("p1")
 let moved = false;
-p1.onclick=function (){
-  if(!moved){
-    p1.style.translate = "0vw"
-    p1.style.rotate="0deg"
-    p1.style.backgroundColor = "darkblue"
-  }else {
-    p1.style.translate = "90vw"
-    p1.style.rotate="50deg"
-    p1.style.backgroundColor = "blue"
-  }
-  p1.style.transition="translate 1s"
-  moved=!moved
-}
 
+window.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('wrapper').addEventListener('click', async (event) => {
+    const el = event.target;
+    const squareColor = el.dataset.square;
+    switch (squareColor) {
+      case 'blue':
+        if (moved) {
+          el.classList.replace('p1_activ', 'p1_inactiv')
+        } else {
+          el.classList.replace('p1_inactiv', 'p1_activ')
+        }
+        moved = !moved
+        break
 
-let p2 = document.getElementById("p2")
-p2.onclick=function (){
-  p2.style.transition="translate 1s"
-    p2.style.translate = "90vw"
-  p2.style.backgroundColor = "blueviolet"
-  p2.style.borderRadius = "50%"
+      case 'darkblue':
+        el.classList.replace('p2_inactiv', 'p2_activ')
+        await translate1(el, 'p2_activ', 'p2_inactiv');
+        break
 
-  setTimeout(function () {
-    p2.style.translate = "0vw"
-    p2.style.backgroundColor = "blue"
-    p2.style.borderRadius = "0%"
-  }, 2000);
-}
+      case 'purple':
+        el.classList.replace('p3_top_left', 'p3_bottom_left');
+        await translate1(el, 'p3_bottom_left', 'p3_bottom_right');
+        await translate1(el,  'p3_bottom_right', 'p3_top_right');
+        await translate1(el,   'p3_top_right', 'p3_top_left');
+        break
+    }
+  })
+});
 
-let p3 = document.getElementById("p3")
-p3.onclick= async function () {
-  p3.style.transition = "all 1s"
-  p3.style.translate = "0 300px"
-  p3.style.opacity="0.5"
-  await  translate1("90vw", "300px",0.5);
-  await translate1("90vw", "0px", 1);
-  await translate1("0vw", "0px",1);
-
-}
-const translate1 = (x, y, o)=> {
+const translate1 = (el, style1, style2)=> {
   return new Promise((resolve)=>setTimeout(function () {
-    resolve(p3.style.translate=x+" "+y, p3.style.opacity=o);
+    resolve(el.classList.replace(style1, style2));
   }, 2000));
 }
